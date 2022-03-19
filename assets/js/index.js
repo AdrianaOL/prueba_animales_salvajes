d899125e14d2e61ccf3324f60eefad07ed2fac2f
Adriana Oviedo <oviedolopezadriana13@gmail.com>
2022-03-19 12:21:13 -0300

    url add

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,15 @@
-// Importacion de Clases hijas de Animal
 import { Serpiente } from './Serpiente.js'
 import { Lobo } from './Lobo.js'
+import { Oso } from './Oso.js'
 import { Aguila } from './Aguila.js'
 import { Leon } from './Leon.js'
-import { Oso } from './Oso.js'
 
-// Creando constante URL para alamacenar el archivo json
-const url = 'http://localhost:5500/animales.json'
-// Funcion asincrona IIFE
+const url = 'https://adrianaol.github.io/prueba_animales_salvajes/animales.json'
 ;(async () => {
   const request = async (url) => {
     const response = await fetch(url)
     const result = await response.json()
-
+    const listadoAnimales = []
     const getElementBySelector = (selector) => document.querySelector(selector)
     const getValueBySelector = (selector) =>
       getElementBySelector(selector).value
@@ -25,19 +22,6 @@ const url = 'http://localhost:5500/animales.json'
       const preview = getElementBySelector('#preview')
       const animales = getElementBySelector('#Animales')
 
-      for (let i = 0; i < result.animales.length; i++) {
-        if (result.animales[i].name === animal) {
-          preview.innerHTML = `<img src='assets/imgs/${result.animales[i].imagen}' class='h-100 w-75' style='object-fit: cover;'>`
-          animales.innerHTML += `
-                           <div class='card w-25 h-50' style='width: 18rem;'>
-                                   <img src='assets/imgs/${result.animales[i].imagen}' class='card-img-top h-75' style='object-fit: cover;'>
-                               <div class='card-body bg-secondary h-25 p-0 d-flex align-content-center justify-content-center'>
-                                   <img class='audio audio${result.animales[i].name}' type='button' src='assets/imgs/audio.svg' class='w-25'>
-                               </div>
-                           </div>`
-        }
-      }
-
       if (animal === 'Leon') {
         const leon = new Leon(
           animal,
@@ -46,6 +30,7 @@ const url = 'http://localhost:5500/animales.json'
           comentarios,
           result.animales[0].sonido
         )
+        listadoAnimales.push(leon)
       } else if (animal === 'Lobo') {
         const lobo = new Lobo(
           animal,
@@ -54,9 +39,7 @@ const url = 'http://localhost:5500/animales.json'
           comentarios,
           result.animales[1].sonido
         )
-        document.querySelector('.audioLobo').addEventListener('click', () => {
-          Lobo.aullar()
-        })
+        listadoAnimales.push(lobo)
       } else if (animal === 'Oso') {
         const oso = new Oso(
           animal,
@@ -65,6 +48,7 @@ const url = 'http://localhost:5500/animales.json'
           comentarios,
           result.animales[2].sonido
         )
+        listadoAnimales.push(oso)
       } else if (animal === 'Serpiente') {
         const serpiente = new Serpiente(
           animal,
@@ -73,6 +57,7 @@ const url = 'http://localhost:5500/animales.json'
           comentarios,
           result.animales[3].sonido
         )
+        listadoAnimales.push(serpiente)
       } else if (animal === 'Aguila') {
         const aguila = new Aguila(
           animal,
@@ -81,7 +66,21 @@ const url = 'http://localhost:5500/animales.json'
           comentarios,
           result.animales[4].sonido
         )
+        listadoAnimales.push(aguila)
       }
+      preview.innerHTML = `<img src='assets/imgs/${listadoAnimales[listadoAnimales.length-1].img}' class='h-100 w-75' style='object-fit: cover;'>`
+      animales.innerHTML = ''
+      listadoAnimales.forEach((animal) => {
+        console.log(animal)
+       
+        animales.innerHTML += `
+                           <div class='card w-25 h-50' style='width: 18rem;'>
+                                   <img src='assets/imgs/${animal.img}' class='card-img-top h-75' style='object-fit: cover;'>
+                               <div class='card-body bg-secondary h-25 p-0 d-flex align-content-center justify-content-center'>
+                                   <img class='' type='button' src='assets/imgs/audio.svg' class='w-25'>
+                               </div>
+                           </div>`
+      })
       document.querySelector('#animal').value = ''
       document.querySelector('#edad').value = ''
       document.querySelector('#comentarios').value = ''
